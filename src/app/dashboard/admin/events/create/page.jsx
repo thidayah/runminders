98b8d0acb1c2c0d511ec559d8d214efbf6073a37
taufik <taufik@codelabs.co.id>
2026-03-15 14:25:51 +0700

    feat: create manage event module

---
+++ src/app/dashboard/admin/events/create/page.jsx
@@ -0,0 +1,8 @@
+'use client'
+
+import EventFormPage from "../[id]/page"
+
+export default function CreateEventPage() {
+  // This will use the same component with no ID (create mode)
+  return <EventFormPage />
+}
