3b674aef971ad1fd2e16881bd6b30af37154ba6b
taufik <taufik@codelabs.co.id>
2026-05-08 16:54:57 +0700

    update: landing maintenance layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,24 @@
-import Layout from '@/components/layout/Layout'
-import HeroSection from '@/components/home/HeroSection'
-import AboutSection from '@/components/home/AboutSection'
-import EventsSection from '@/components/home/EventsSection'
-import CategoryFilter from '@/components/home/CategoryFilter'
-import PartnersSection from '@/components/home/PartnersSection'
-import ReviewsSection from '@/components/home/ReviewsSection'
+// import Layout from '@/components/layout/Layout'
+// import HeroSection from '@/components/home/HeroSection'
+// import AboutSection from '@/components/home/AboutSection'
+// import EventsSection from '@/components/home/EventsSection'
+// import CategoryFilter from '@/components/home/CategoryFilter'
+// import PartnersSection from '@/components/home/PartnersSection'
+// import ReviewsSection from '@/components/home/ReviewsSection'
+
+import MaintenanceSection from "@/components/home/MaintenanceSection";
 
 export default function Home() {
   return (
-    <Layout>
-      <HeroSection />
-      <AboutSection />
-      <EventsSection />
-      {/* <CategoryFilter /> */}
-      {/* <PartnersSection /> */}
-      <ReviewsSection />
-    </Layout>
+    // <Layout>
+    //   <HeroSection />
+    //   <AboutSection />
+    //   <EventsSection />
+    //   {/* <CategoryFilter /> */}
+    //   {/* <PartnersSection /> */}
+    //   <ReviewsSection />
+    // </Layout>
+
+    <MaintenanceSection />
   )
 }
